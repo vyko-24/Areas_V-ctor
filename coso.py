49ebfcde73737bb71e79058fc1c122ddef3d4c4e
vyko-24 <133275788+vyko-24@users.noreply.github.com>
2025-06-10 17:02:11 -0600

    coso
    coso

---
+++ coso.py
@@ -0,0 +1,30 @@
+
+print('Que figura desea calcular? \n 1.- Triangulo \n 2.- Cuadrado \n 3.- Circulo \n 4.- Rectángulo')
+
+coso = input()
+
+if coso == '1':
+    print("Inserte la base y luego la altura")
+    bs = int(input())
+    h = int(input())
+    print("El area de tu triangulo es:")
+    print(bs*h/2)
+
+if coso == '2':
+    print("Inserte el lado de uno de los lados")
+    ld = int(input())
+    print("El area de tu cuadrado es:")
+    print(ld*ld)
+
+if coso == '3':
+    print("Inserte el valor del radio")
+    rd = int(input())
+    print("El area de tu círculo es:")
+    print(3.1416*(rd*rd))
+
+if coso == '4':
+    print("Inserte el valor de la base y luego de la altura")
+    bd = int(input())
+    h = int(input())
+    print("El area de tu rectángulo es:")
+    print(bd*h)
